4317b79af8cae7dcf7c9ec74563d0bdb70bde847
MD. Sohail <mdsohail88008@gmail.com>
2026-03-31 15:20:09 +0530

    Remove S3 bucket ownership controls resource
    Removed ownership controls for the S3 bucket.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -18,14 +18,6 @@ resource "aws_s3_bucket_public_access_block" "django_media" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_ownership_controls" "django_media" {
-  bucket = aws_s3_bucket.django_media.id
-
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-}
-
 resource "aws_s3_bucket_versioning" "django_media" {
   bucket = aws_s3_bucket.django_media.id
 
