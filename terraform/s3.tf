bbbc4d25d34f79cb0469c8908d0fded56667364a
MD. Sohail <mdsohail88008@gmail.com>
2026-03-31 14:57:21 +0530

    Enhance S3 bucket security and enable versioning

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -12,43 +12,34 @@ resource "aws_s3_bucket" "django_media" {
 resource "aws_s3_bucket_public_access_block" "django_media" {
   bucket = aws_s3_bucket.django_media.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_ownership_controls" "django_media" {
   bucket = aws_s3_bucket.django_media.id
 
   rule {
-    object_ownership = "BucketOwnerPreferred"
+    object_ownership = "BucketOwnerEnforced"
   }
 }
 
-resource "aws_s3_bucket_acl" "django_media" {
-  depends_on = [
-    aws_s3_bucket_public_access_block.django_media,
-    aws_s3_bucket_ownership_controls.django_media
-  ]
-
+resource "aws_s3_bucket_versioning" "django_media" {
   bucket = aws_s3_bucket.django_media.id
-  acl    = "public-read"
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
-resource "aws_s3_bucket_policy" "django_media_policy" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "django_media" {
   bucket = aws_s3_bucket.django_media.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.django_media.arn}/*"
-      }
-    ]
-  })
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
